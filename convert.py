323036941a027a5f45bfa0cb8e3d9fa202eb6985
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-03-05 10:28:29 +0100

    Allow skipping faulty PDFs

--- convert.py
+++ convert.py
@@ -12,12 +12,20 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', default=DEFAULT_DESTINATION,
                         help='Source directory with publication PDFs')
+    parser.add_argument('--strict', dest='ignore_errors', action='store_const',
+                        default=True, const=False,
+                        help='If specified, throw on PDF open error')
     return parser.parse_args()
 
 
-def convert(path: str):
+def convert(path: str, ignore_errors: bool = False):
     with open(path, 'rb') as infile:
-        pdf = pdftotext.PDF(infile)
+        try:
+            pdf = pdftotext.PDF(infile)
+        except pdftotext.Error:
+            if ignore_errors:
+                return
+            raise
     out_path = "{0}.txt".format(os.path.splitext(path)[0])
     with open(out_path, 'w') as outfile:
         for i in range(len(pdf)):
@@ -30,7 +38,7 @@ def main():
     pattern = os.path.join(args.source, '**', '*.pdf')
     queue = glob.glob(pattern, recursive=True)
     for path in tqdm(queue, 'conversion'):
-        convert(path)
+        convert(path, ignore_errors=args.ignore_errors)
 
 
 if __name__ == '__main__':
