0e7b333f15569cfe889297e01b8a3b4431ae2836
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-08-28 13:22:37 +0200

    Migrate convert.py to pdfminer.six

--- convert.py
+++ convert.py
@@ -1,44 +1,51 @@
 import argparse
+from functools import partial
 import glob
+import logging
+from multiprocessing import Pool
 import os
+import subprocess
 
-import pdftotext
 from tqdm import tqdm
-
-from utils import DEFAULT_DESTINATION
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', default=DEFAULT_DESTINATION,
+    parser.add_argument('--source', required=True,
                         help='Source directory with publication PDFs')
-    parser.add_argument('--strict', dest='ignore_errors', action='store_const',
-                        default=True, const=False,
-                        help='If specified, throw on PDF open error')
+    parser.add_argument('--destination', required=True,
+                        help='Destination directory for publication TXTs')
     return parser.parse_args()
 
 
-def convert(path: str, ignore_errors: bool = False):
-    with open(path, 'rb') as infile:
-        try:
-            pdf = pdftotext.PDF(infile)
-        except pdftotext.Error:
-            if ignore_errors:
-                return
-            raise
-    out_path = "{0}.txt".format(os.path.splitext(path)[0])
-    with open(out_path, 'w') as outfile:
-        for i in range(len(pdf)):
-            normalized = pdf[i].encode('ascii', errors='replace').decode('ascii')
-            outfile.write(normalized)
+def convert_pdf(src: str, dst: str):
+    completed = subprocess.run(['pdf2txt.py', '-o', dst, src])
+    if completed.returncode:
+        logging.warning(f"{src} conversion failed.")
+
+
+def prepare_destination(src: str, dst_root: str):
+    head, fname = os.path.split(src)
+    _, category = os.path.split(head)
+    pub_name, _ = os.path.splitext(fname)
+    dst_dir = os.path.join(dst_root, category)
+    os.makedirs(dst_dir, exist_ok=True)
+    dst = os.path.join(dst_dir, f"{pub_name}.txt")
+    return dst
+
+
+def convert(src: str, dst_root: str):
+    dst = prepare_destination(src, dst_root)
+    convert_pdf(src, dst)
 
 
 def main():
     args = parse_args()
     pattern = os.path.join(args.source, '**', '*.pdf')
     queue = glob.glob(pattern, recursive=True)
-    for path in tqdm(queue, 'conversion'):
-        convert(path, ignore_errors=args.ignore_errors)
+    _convert = partial(convert, dst_root=args.destination)
+    with Pool(20) as pool:
+        pool.map(_convert, tqdm(queue, 'conversion'))
 
 
 if __name__ == '__main__':
