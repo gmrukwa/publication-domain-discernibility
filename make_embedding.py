208a53b4925a3581fe6a76150374f4fb26586aee
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2019-08-30 02:11:41 +0200

    Create script for embedding

---
+++ make_embedding.py
@@ -0,0 +1,94 @@
+import argparse
+import json
+
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', required=True,
+                        help='data source')
+    parser.add_argument('--categories', required=True,
+                        help='categories source')
+    parser.add_argument('--embedding', required=True,
+                        help='embedding destination')
+    parser.add_argument('--words', required=True,
+                        help='words destination')
+    parser.add_argument('--N', default=5, type=int,
+                        help='number of characteristic words')
+    return parser.parse_args()
+
+
+def load_data(fname):
+    with open(fname, 'rb') as infile:
+        return joblib.load(infile)
+
+
+def save_data(data, fname):
+    with open(fname, 'wb') as outfile:
+        joblib.dump(data, outfile)
+
+
+def load_json(fname):
+    with open(fname) as infile:
+        return json.load(infile)
+
+
+def save_json(obj, fname):
+    with open(fname, 'w') as outfile:
+        json.dump(data, outfile, sort_keys=True, indent=2)
+
+
+def select_categories(data, categories):
+    selection = np.zeros((data.shape[0],), dtype=bool)
+    for category in categories:
+        selection = np.logical_or(selection, data.domain == category)
+    return data[selection].copy()
+
+
+def effect_size(first, second, axis=0):
+    mu1 = first.mean(axis=axis)
+    mu2 = second.mean(axis=axis)
+    s1 = np.var(first, axis=axis)
+    s2 = np.var(second, axis=axis)
+    n1, n2 = first.shape[axis], second.shape[axis]
+    s = np.sqrt((n1 * s1 + n2 * s2) / (n1 + n2 - 2))
+    d = mu1 - mu2 / s
+    return d
+
+
+def characteristic_features(data, selector, n: int=5):
+    first, second = data[selector], data[~selector]
+    d = effect_size(first, second)
+    d_sort = np.argsort(d)
+    top = d_sort[-n::-1]
+    bot = d_sort[:n]
+    return bot, top
+
+
+def main():
+    args = parse_args()
+    data = load_data(args.source)
+    categories = load_json(args.categories)
+    selector = select_categories(data, categories)
+    data = data[selector]
+    vectorizer = TfidfVectorizer(ngram_range=(1,3))
+    embedding = vectorizer.fit_transform(data)
+    save_data(embedding, args.embedding)
+    words = vectorizer.get_feature_names()
+    characteristic_words = {}
+    for domain in tqdm(categories):
+        category = data.domain == domain
+        bot, top = characteristic_features(
+            embedding, category, n=args.N)
+        characteristic_words[f'{domain}_up'] = words[top]
+        characteristic_words[f'{domain}_down'] = words[bot]
+    save_json(characteristic_words, args.words)
+
+
+if __name__ == '__main__':
+    main()
